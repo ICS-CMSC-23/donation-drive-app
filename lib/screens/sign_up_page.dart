9d3766505898d4ad2f4edc762f5b8ce67ddfcee3
aerondain <aerondain@up.edu.ph>
2024-05-20 16:51:47 +0800

    ui: updated sign up page design

--- lib/screens/sign_up_page.dart
+++ lib/screens/sign_up_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:email_validator/email_validator.dart';
-import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import '../provider/auth_provider.dart';
 
@@ -367,111 +366,147 @@ class _OrganizationSignUpPageState extends State<OrganizationSignUpPage> {
       },
     );
 
-    final signUpButton = Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16.0),
-      child: ElevatedButton(
-        style: ButtonStyle(
-          backgroundColor: const MaterialStatePropertyAll<Color>(
-              Color.fromARGB(255, 38, 32, 69)),
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(18.0),
-              side: const BorderSide(
-                  color: Color.fromARGB(255, 38, 32, 69), width: 2.0),
-            ),
+    final signUpButton = ElevatedButton(
+      style: ButtonStyle(
+        backgroundColor: const MaterialStatePropertyAll<Color>(
+            Color.fromRGBO(255, 63, 64, 1)),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18.0),
+            side: const BorderSide(
+                color: Color.fromRGBO(255, 63, 64, 1), width: 2.0),
           ),
         ),
-        onPressed: () async {
-          if (_formKey.currentState!.validate()) {
-            await context
-                .read<MyAuthProvider>()
-                .signUp(emailController.text, passwordController.text);
-            if (context.mounted) Navigator.pop(context);
-          }
-        },
-        child: const Text('Sign up',
-            style: TextStyle(color: Color.fromRGBO(48, 61, 78, 1))),
       ),
+      onPressed: () async {
+        if (_formKey.currentState!.validate()) {
+          await context
+              .read<MyAuthProvider>()
+              .signUp(emailController.text, passwordController.text);
+          if (context.mounted) Navigator.pop(context);
+        }
+      },
+      child: const Text('SIGN UP', style: TextStyle(color: Colors.white)),
     );
 
-    final backButton = Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16.0),
-      child: ElevatedButton(
-        style: ButtonStyle(
-          backgroundColor: const MaterialStatePropertyAll<Color>(Colors.white),
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(18.0),
-              side: const BorderSide(
-                  color: Color.fromRGBO(48, 61, 78, 1), width: 2.0),
-            ),
+    final backButton = ElevatedButton(
+      style: ButtonStyle(
+        backgroundColor: const MaterialStatePropertyAll<Color>(Colors.white),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18.0),
+            side: const BorderSide(
+                color: Color.fromRGBO(255, 63, 64, 1), width: 2.0),
           ),
         ),
-        onPressed: () async {
-          Navigator.pop(context);
-        },
-        child: const Text('BACK',
-            style: TextStyle(color: Color.fromARGB(255, 38, 32, 69))),
       ),
+      onPressed: () async {
+        Navigator.pop(context);
+      },
+      child: const Text('BACK',
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(255, 63, 64, 1))),
     );
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Organization Sign Up",
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 25),
-        ),
-      ),
-      backgroundColor: Colors.white,
+      backgroundColor: const Color.fromRGBO(48, 61, 78, 1),
       body: SingleChildScrollView(
         child: Center(
           child: Form(
             key: _formKey,
             child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  firstName,
-                  lastName,
-                  email,
-                  username,
-                  password,
-                  for (var i = 0; i < addressesController.length; i++)
-                    Row(children: [
-                      Expanded(
-                          child: TextFormField(
-                        controller: addressesController[i],
-                        decoration: InputDecoration(
-                          labelText: 'Address ${i + 1}',
-                          border: const OutlineInputBorder(),
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+                child: Card(
+                    child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Welcome!",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      firstName,
+                      const SizedBox(height: 10),
+                      lastName,
+                      const SizedBox(height: 10),
+                      email,
+                      const SizedBox(height: 10),
+                      username,
+                      const SizedBox(height: 10),
+                      password,
+                      const SizedBox(height: 10),
+                      for (var i = 0; i < addressesController.length; i++)
+                        Column(children: [
+                          Row(children: [
+                            Expanded(
+                                child: TextFormField(
+                              controller: addressesController[i],
+                              decoration: InputDecoration(
+                                labelText: 'Address ${i + 1}',
+                                border: const OutlineInputBorder(),
+                              ),
+                            )),
+                            const SizedBox(height: 10),
+                            IconButton(
+                                color: const Color.fromRGBO(255, 63, 64, 1),
+                                icon: const Icon(Icons.playlist_remove_rounded),
+                                onPressed: () {
+                                  setState(() {
+                                    addressesController.removeAt(i);
+                                  });
+                                })
+                          ]),
+                          const SizedBox(height: 10),
+                        ]),
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          foregroundColor:
+                              const MaterialStatePropertyAll<Color>(
+                                  Color.fromRGBO(255, 63, 64,
+                                      1)), // Set the icon/text color
+                          side: const MaterialStatePropertyAll<BorderSide>(
+                              BorderSide(
+                                  color: Color.fromRGBO(255, 63, 64, 1),
+                                  width: 2.0) // Outline color
+                              ),
+                          shape:
+                              MaterialStatePropertyAll<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                  18.0), // Set the border radius if needed
+                            ),
+                          ),
                         ),
-                      )),
-                      IconButton(
-                          icon: const Icon(Icons.remove),
-                          onPressed: () {
-                            setState(() {
-                              addressesController.removeAt(i);
-                            });
-                          })
-                    ]),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        addressesController.add(TextEditingController());
-                      });
-                    },
-                    child: const Text("Add address"),
+                        onPressed: () {
+                          setState(() {
+                            addressesController.add(TextEditingController());
+                          });
+                        },
+                        child: const Text('ADD ADDRESS',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                      const SizedBox(height: 10),
+                      orgName,
+                      const SizedBox(height: 10),
+                      orgProof,
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(child: backButton),
+                          const SizedBox(width: 10),
+                          Expanded(child: signUpButton),
+                        ],
+                      ),
+                    ],
                   ),
-                  orgName,
-                  orgProof,
-                  signUpButton,
-                  backButton,
-                ],
-              ),
-            ),
+                ))),
           ),
         ),
       ),
