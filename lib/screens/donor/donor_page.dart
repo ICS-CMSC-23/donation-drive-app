1da88644c2595d335d3ef82befe237932a68fb1a
Nic <158990276+nickeelodeon@users.noreply.github.com>
2024-06-10 04:49:10 +0800

    feat: added overlay box to display org details

--- lib/screens/donor/donor_page.dart
+++ lib/screens/donor/donor_page.dart
@@ -1,47 +1,28 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:project_jdvillamin/screens/sign_in_page.dart';
 import 'package:provider/provider.dart';
 import '../../providers/organization_provider.dart';
-import '../../providers/auth_provider.dart'; // Import the auth provider
 import 'donor_profile.dart';
 import 'donate_form.dart';
 import '../../models/organization_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'donationpage.dart';
 
-class DonorPage extends StatefulWidget {
-  const DonorPage({super.key});
-
-  @override
-  State<DonorPage> createState() => _DonorPageState();
-}
-
-class _DonorPageState extends State<DonorPage> {
-  void approveOrganization(Organization organization) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => DonationFormPage(organization: organization),
-      ),
-    );
-
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text("Organization chosen"),
-      ),
-    );
-  }
+class DonorPage extends StatelessWidget {
+  const DonorPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final authProvider = Provider.of<MyAuthProvider>(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("Donor Homepage"),
       ),
-      body: Consumer<OrganizationListProvider>(
-        builder: (context, provider, _) {
-          return StreamBuilder<QuerySnapshot>(
-            stream: provider.organizations,
+      drawer: _buildDrawer(context),
+      body: Stack(
+        children: [
+          StreamBuilder<QuerySnapshot>(
+            stream: Provider.of<OrganizationListProvider>(context).organizations,
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
                 return const Center(child: CircularProgressIndicator());
@@ -52,7 +33,8 @@ class _DonorPageState extends State<DonorPage> {
               } else {
                 var organizations = snapshot.data!.docs.map((doc) {
                   return Organization.fromJson(
-                      doc.data() as Map<String, dynamic>);
+                    doc.data() as Map<String, dynamic>,
+                  );
                 }).toList();
 
                 return SingleChildScrollView(
@@ -62,8 +44,9 @@ class _DonorPageState extends State<DonorPage> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: organizations.map((org) {
                         return _buildOrganizationCard(
+                          context,
                           org,
-                          () => approveOrganization(org),
+                          () => approveOrganization(context, org),
                         );
                       }).toList(),
                     ),
@@ -71,29 +54,112 @@ class _DonorPageState extends State<DonorPage> {
                 );
               }
             },
-          );
-        },
+          ),
+        ],
       ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            heroTag: 'profileButton',
-            child: const Icon(Icons.account_circle),
-            onPressed: () {
+    );
+  }
+
+  void approveOrganization(BuildContext context, Organization organization) {
+    // Navigate to DonationFormPage with the selected organization
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => DonationFormPage(organization: organization),
+      ),
+    );
+
+    // Show snackbar message for approval
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text("Organization chosen"),
+      ),
+    );
+  }
+
+  Widget _buildOrganizationCard(BuildContext context, Organization organization, VoidCallback onApprove) {
+    return GestureDetector(
+      onTap: () {
+        _showOverlay(context, organization, onApprove);
+      },
+      child: Card(
+        elevation: 4,
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                organization.organizationName,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                organization.about ?? "No description available",
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text(
+              'Menu',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const Text('View Profile'),
+            onTap: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => DonorProfile()),
+                MaterialPageRoute(builder: (context) => const DonorProfile()),
               );
             },
           ),
-          const SizedBox(height: 16),
-          FloatingActionButton(
-            heroTag: 'signOutButton',
-            child: const Icon(Icons.exit_to_app),
-            onPressed: () async {
-              await authProvider.signOut();
-              Navigator.pop(context);
+          ListTile(
+            leading: const Icon(Icons.history),
+            title: const Text('View Donation History'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const DonationHistoryPage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('Log Out'),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const SignInPage()),
+              );
             },
           ),
         ],
@@ -101,43 +167,150 @@ class _DonorPageState extends State<DonorPage> {
     );
   }
 
-  Widget _buildOrganizationCard(
-      Organization organization, VoidCallback onApprove) {
-    return Card(
-      elevation: 4,
-      margin: const EdgeInsets.symmetric(vertical: 10),
-      child: Padding(
-        padding: const EdgeInsets.all(20),
+  void _showOverlay(BuildContext context, Organization organization, VoidCallback onApprove) {
+  OverlayEntry? overlay;
+
+  // Create a controller for the animation
+  AnimationController? controller;
+
+  // Define the animation
+  Animation<double>? animation;
+
+  // Function to remove overlay and dispose animation controller
+  void removeOverlayAndDispose() {
+    overlay?.remove();
+    if (controller != null) {
+      controller.dispose();
+    }
+  }
+
+  controller = AnimationController(
+    duration: const Duration(milliseconds: 300), // Adjust duration as needed
+    vsync: Navigator.of(context),
+  );
+
+  animation = CurvedAnimation(
+    parent: controller,
+    curve: Curves.easeInOut,
+  );
+
+  // Start the animation
+  controller.forward();
+
+  overlay = OverlayEntry(
+    builder: (context) => AnimatedBuilder(
+      animation: animation!,
+      builder: (context, child) {
+        return Opacity(
+          opacity: animation!.value,
+          child: Stack(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  controller!.reverse(); // Reverse the animation when overlay is tapped
+                },
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                  child: Container(
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ),
+              ),
+              Center(
+                child: Material(
+                  color: Colors.transparent,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * .75,
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          organization.organizationName,
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 15),
+                        _buildDetailBox('Address', organization.addresses.join(", ")),
+                        const SizedBox(height: 10),
+                        _buildDetailBox('Contact Number', organization.contactNo),
+                        const SizedBox(height: 10),
+                        _buildDetailBox('About', organization.about ?? "No description available"),
+                        const SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () {
+                            onApprove();
+                            removeOverlayAndDispose(); // Reverse the animation when button is pressed
+                          },
+                          child: const Text("Select this organization"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    ),
+  );
+
+  Overlay.of(context).insert(overlay);
+
+  // Add listener to dispose the animation controller when the overlay is removed
+  controller.addStatusListener((status) {
+    if (status == AnimationStatus.dismissed) {
+      removeOverlayAndDispose();
+    }
+  });
+}
+
+  Widget _buildDetailBox(String title, String detail) {
+    return SizedBox(
+      width: 200, // Set a fixed width for each detail box
+      height: 100, // Set a fixed height for each detail box
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 4,
+              offset: Offset(2, 2),
+            ),
+          ],
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
+          children: [
             Text(
-              organization.organizationName,
+              title,
               style: const TextStyle(
-                fontSize: 18,
+                fontSize: 16,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(height: 10),
+            const SizedBox(height: 5),
             Text(
-              organization.about ?? "No description available",
-              style: const TextStyle(
-                fontSize: 16,
-              ),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                  onPressed: organization.isOpen ? onApprove : null,
-                  child: const Text("Select this organization"),
-                )
-              ],
+              detail,
+              style: const TextStyle(fontSize: 14),
             ),
           ],
         ),
       ),
     );
   }
+
 }
+
