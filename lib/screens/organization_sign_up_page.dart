7e7a7e98ef051553f41f084fc69a3af4a46ad45b
aerondain <aerondain@up.edu.ph>
2024-06-10 11:56:03 +0800

    ui: updated organization_sign_up_page ui

--- lib/screens/organization_sign_up_page.dart
+++ lib/screens/organization_sign_up_page.dart
@@ -129,153 +129,250 @@ class _OrganizationSignUpPageState extends State<OrganizationSignUpPage> {
       },
     );
 
-    final signUpButton = Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16.0),
-      child: ElevatedButton(
-        style: ButtonStyle(
-          backgroundColor: const MaterialStatePropertyAll<Color>(
-              Color.fromARGB(255, 38, 32, 69)),
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(18.0),
-              side: const BorderSide(
-                  color: Color.fromARGB(255, 38, 32, 69), width: 2.0),
-            ),
+    final signUpButton = ElevatedButton(
+      style: ButtonStyle(
+        backgroundColor: const MaterialStatePropertyAll<Color>(
+            Color.fromRGBO(255, 63, 64, 1)),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18.0),
+            side: const BorderSide(
+                color: Color.fromRGBO(255, 63, 64, 1), width: 2.0),
           ),
         ),
-        onPressed: () async {
-          if (_formKey.currentState!.validate()) {
-            await context
-                .read<MyAuthProvider>()
-                .signUp(usernameController.text, passwordController.text);
-
-            int organizationCount = await context
-                .read<OrganizationListProvider>()
-                .getOrganizationCount();
-
-            // Upload file and get URL
-            final proofUrl = await uploadFile();
-
-            context
-                .read<OrganizationListProvider>()
-                .addOrganization(Organization(
-                  userId: organizationCount,
-                  name:
-                      '${firstNameController.text} ${lastNameController.text}',
-                  username: usernameController.text,
-                  password: passwordController.text,
-                  addresses: addressesController.map((c) => c.text).toList(),
-                  contactNo: contactNoController.text,
-                  organizationName: organizationNameController.text,
-                  proofsOfLegitimacy: proofUrl != null ? [proofUrl] : [],
-                ));
-            if (context.mounted) Navigator.pop(context);
-          }
-        },
-        child: const Text('Sign up', style: TextStyle(color: Colors.white)),
       ),
+      onPressed: () async {
+        if (_formKey.currentState!.validate()) {
+          await context
+              .read<MyAuthProvider>()
+              .signUp(usernameController.text, passwordController.text);
+
+          int organizationCount = await context
+              .read<OrganizationListProvider>()
+              .getOrganizationCount();
+
+          // Upload file and get URL
+          final proofUrl = await uploadFile();
+
+          context.read<OrganizationListProvider>().addOrganization(Organization(
+                userId: organizationCount,
+                name: '${firstNameController.text} ${lastNameController.text}',
+                username: usernameController.text,
+                password: passwordController.text,
+                addresses: addressesController.map((c) => c.text).toList(),
+                contactNo: contactNoController.text,
+                organizationName: organizationNameController.text,
+                proofsOfLegitimacy: proofUrl != null ? [proofUrl] : [],
+              ));
+          if (context.mounted) Navigator.pop(context);
+        }
+      },
+      child: const Text('SIGN UP', style: TextStyle(color: Colors.white)),
     );
 
-    final backButton = Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16.0),
-      child: ElevatedButton(
-        style: ButtonStyle(
-          backgroundColor: const MaterialStatePropertyAll<Color>(Colors.white),
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(18.0),
-              side: const BorderSide(
-                  color: Color.fromARGB(255, 38, 32, 69), width: 2.0),
-            ),
+    final backButton = ElevatedButton(
+      style: ButtonStyle(
+        backgroundColor: const MaterialStatePropertyAll<Color>(Colors.white),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18.0),
+            side: const BorderSide(
+                color: Color.fromRGBO(255, 63, 64, 1), width: 2.0),
           ),
         ),
-        onPressed: () async {
-          Navigator.pop(context);
-        },
-        child: const Text('Back',
-            style: TextStyle(color: Color.fromARGB(255, 38, 32, 69))),
       ),
+      onPressed: () async {
+        Navigator.pop(context);
+      },
+      child: const Text('BACK',
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(255, 63, 64, 1))),
     );
 
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(48, 61, 78, 1),
         title: const Text(
           "Organization Sign Up",
           textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 25),
+          style: TextStyle(
+            fontSize: 25, fontWeight: FontWeight.bold, // Bold text
+            color: Colors.white, // White text color
+          ),
         ),
       ),
-      backgroundColor: Colors.white,
+      backgroundColor: const Color.fromRGBO(48, 61, 78, 1),
       body: SingleChildScrollView(
         child: Center(
           child: Form(
             key: _formKey,
             child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  firstName,
-                  lastName,
-                  username,
-                  password,
-                  for (var i = 0; i < addressesController.length; i++)
-                    Row(children: [
-                      Expanded(
-                          child: TextFormField(
-                        controller: addressesController[i],
-                        decoration: InputDecoration(
-                          labelText: 'Address ${i + 1}',
-                          border: const OutlineInputBorder(),
-                        ),
-                      )),
-                      IconButton(
-                          icon: const Icon(Icons.remove),
-                          onPressed: () {
-                            setState(() {
-                              addressesController.removeAt(i);
-                            });
-                          })
-                    ]),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        addressesController.add(TextEditingController());
-                      });
-                    },
-                    child: const Text("Add address"),
-                  ),
-                  organizationName,
-                  for (var i = 0; i < proofsOfLegitimacyController.length; i++)
-                    Row(children: [
-                      ElevatedButton(
-                        onPressed: selectFile,
-                        child: const Text('Select File'),
+                padding: const EdgeInsets.all(20),
+                child: Card(
+                    child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Welcome!",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
                       ),
-                      Text(pickedFile?.name ?? 'No file selected'),
-                      IconButton(
-                          icon: const Icon(Icons.remove),
-                          onPressed: () {
-                            setState(() {
-                              proofsOfLegitimacyController.removeAt(i);
-                            });
-                          })
-                    ]),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        proofsOfLegitimacyController
-                            .add(TextEditingController());
-                      });
-                    },
-                    child: const Icon(Icons.add),
+                      const SizedBox(height: 10),
+                      firstName,
+                      const SizedBox(height: 10),
+                      lastName,
+                      const SizedBox(height: 10),
+                      username,
+                      const SizedBox(height: 10),
+                      password,
+                      const SizedBox(height: 10),
+                      for (var i = 0; i < addressesController.length; i++)
+                        Column(children: [
+                          Row(children: [
+                            Expanded(
+                                child: TextFormField(
+                              controller: addressesController[i],
+                              decoration: InputDecoration(
+                                labelText: 'Address ${i + 1}',
+                                border: const OutlineInputBorder(),
+                              ),
+                            )),
+                            const SizedBox(height: 10),
+                            IconButton(
+                                color: const Color.fromRGBO(255, 63, 64, 1),
+                                icon: const Icon(Icons.playlist_remove_rounded),
+                                onPressed: () {
+                                  setState(() {
+                                    addressesController.removeAt(i);
+                                  });
+                                })
+                          ]),
+                          const SizedBox(height: 10),
+                        ]),
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          foregroundColor:
+                              const MaterialStatePropertyAll<Color>(
+                                  Color.fromRGBO(255, 63, 64,
+                                      1)), // Set the icon/text color
+                          side: const MaterialStatePropertyAll<BorderSide>(
+                              BorderSide(
+                                  color: Color.fromRGBO(255, 63, 64, 1),
+                                  width: 2.0) // Outline color
+                              ),
+                          shape:
+                              MaterialStatePropertyAll<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            addressesController.add(TextEditingController());
+                          });
+                        },
+                        child: const Text('ADD ADDRESS',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                      const SizedBox(height: 10),
+                      organizationName,
+                      const SizedBox(height: 10),
+                      for (var i = 0;
+                          i < proofsOfLegitimacyController.length;
+                          i++)
+                        Row(children: [
+                          Container(
+                            width: 120,
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    const MaterialStatePropertyAll<Color>(
+                                  Color.fromRGBO(48, 61, 78, 1),
+                                ), // Set the icon/text color
+                                side:
+                                    const MaterialStatePropertyAll<BorderSide>(
+                                        BorderSide(
+                                  color: Color.fromRGBO(48, 61, 78, 1),
+                                ) // Outline color
+                                        ),
+                                shape: MaterialStatePropertyAll<
+                                    RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                  ),
+                                ),
+                                padding:
+                                    const MaterialStatePropertyAll<EdgeInsets>(
+                                  EdgeInsets.symmetric(
+                                      horizontal: 0,
+                                      vertical: 0), // Adjust padding here
+                                ),
+                              ),
+                              onPressed: selectFile,
+                              child: const Text('BROWSE FILES',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  )),
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          Text(pickedFile?.name ?? 'No file selected'),
+                          IconButton(
+                              color: const Color.fromRGBO(48, 61, 78, 1),
+                              icon: const Icon(Icons.playlist_remove_rounded),
+                              onPressed: () {
+                                setState(() {
+                                  proofsOfLegitimacyController.removeAt(i);
+                                });
+                              })
+                        ]),
+                      const SizedBox(height: 10),
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          foregroundColor:
+                              const MaterialStatePropertyAll<Color>(
+                            Color.fromRGBO(48, 61, 78, 1),
+                          ), // Set the icon/text color
+                          side: const MaterialStatePropertyAll<BorderSide>(
+                              BorderSide(
+                                  color: Color.fromRGBO(48, 61, 78, 1),
+                                  width: 2.0) // Outline color
+                              ),
+                          shape:
+                              MaterialStatePropertyAll<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            proofsOfLegitimacyController
+                                .add(TextEditingController());
+                          });
+                        },
+                        child: const Text('ADD PROOF OF LEGITIMACY',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(child: backButton),
+                          const SizedBox(width: 10),
+                          Expanded(child: signUpButton),
+                        ],
+                      ),
+                    ],
                   ),
-                  signUpButton,
-                  backButton,
-                ],
-              ),
-            ),
+                ))),
           ),
         ),
       ),
